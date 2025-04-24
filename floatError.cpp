501ffe85fe4daf3691fedbd7cd7a1965b857c15b
ZIBA99 <97379804+ZIBA99@users.noreply.github.com>
2025-04-24 13:45:01 +0900

    Add files via upload

---
+++ floatError.cpp
@@ -0,0 +1,13 @@
+//#include <stdio.h>//부동 수동점 오차 실습
+
+//int main()
+//{
+	//float f = 0.00F;
+	//int i = 0;
+	//for (f = 0.0; f < 100.0f; f += 0.1f, i++) {
+		//printf("f : %f\n", f);//f로 가른 작업 수행
+	//}
+
+	//printf("i : % d\n", i);
+	//return 0;
+//}
